2b70dfa32a19ac21d95505c0bd43d7b221eeb218
Jonathan McCann <jonathan.mccann@liferay.com>
2016-09-16 12:42:25 -0700

    Uncomment background scrolling

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@ void gameLoop() {
 
 	while (1) {
 		// Trigger the background to be updated
-		//scrollBackground();
+		scrollBackground();
 
 		// Pass the input from the joypad to player.c to update
 		// the player and shot's location
